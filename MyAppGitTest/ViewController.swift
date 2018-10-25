6d0a79b82b1ebcb32e8bd0b6e1d8a7014dce98b3
= <=>
2018-10-25 15:19:21 +0900

    first commit

---
+++ MyAppGitTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyAppGitTest
+//
+//  Created by AiTH2 on 2018/10/25.
+//  Copyright © 2018 AiTH Kimura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
